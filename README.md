019389d62ffc56af96edf33d48b3b70a1bff938d
Dennis Ploeger <develop@dieploegers.de>
2021-03-14 13:15:01 +0100

    feat: Added first version 0.1.0

---
+++ README.md
@@ -0,0 +1,39 @@
+![Speedy Gonzales logo](logo.png)
+
+# Speedy Gonzales - Custom software mouse cursor addon for Godot
+
+## Introduction
+
+*Speedy Gonzales* is an easy custom software mouse cursor addon for Godot. It uses the `default_mouse_cursor_shape` property of [Godot controls](https://docs.godotengine.org/en/stable/classes/class_control.html) to show custom mouse cursors when hovering over them.
+
+It's used as part of the *Egoventure Game Template* together with the [Egoventure](https://github.com/deep-entertainment/egoventure) and [Parrot](https://github.com/deep-entertainment/parrot) addons as a basis for first person point and click adventure games such as the Carol Reed series by [Mdna games](http://mdna-games.com/) since 2021.
+
+## Usage
+
+*Speedy Gonzales* relies on Control nodes and their `default_mouse_cursor_shape` property, but can also used without them.
+
+To set a custom mouse cursor for one of the possible cursor shapes (see the CursorShape enum in [the input class](https://docs.godotengine.org/en/stable/classes/class_input.html)), use the following code:
+
+```gdscript
+Speedy.set_custom_mouse_cursor(
+    preload("res://cursors/hand.png"),
+    Input.CURSOR_POINTING_HAND,
+    Vector2(10, 10)
+)
+```
+
+This example would set a custom hand image for the pointing hand cursor shape with a hotspot at 10 x and 10 y.
+
+Every time the mouse cursor is over a control that has `default_mouse_cursor_shape` set to `Input.CURSOR_POINTING_HAND`, Speedy will change the mouse cursor to the hand image.
+
+**Be aware**, that *Speedy Gonzales* completely takes over mouse control, so all mouse cursor shapes need to be controlled by it.
+
+## Usage without Control nodes
+
+If you only want to control the mouse cursor shape by code without relying on Control nodes, you can set
+
+```gdscript
+Speedy.keep_shape = true
+```
+
+Speedy will not change the mouse cursor shape after that.
---
+++ addons/speedy_gonzales/nodes/speedy.gd
@@ -0,0 +1,102 @@
+# Speedy mouse cursor handling
+extends CanvasLayer
+
+
+# A dictionary of mouse cursor hotspots for cursor shapes
+var hotspots: Dictionary
+
+# A dictionary of mouse cursor textures for cursor shapes
+var textures: Dictionary
+
+# The current mouse cursors hotspot
+var current_hotspot: Vector2
+
+# The current shape
+var current_shape
+
+# Wether the mouse cursor is hidden currently
+var hidden: bool setget _set_hidden
+
+# Keep the current cursor shape and don't update it
+var keep_shape: bool = false
+
+
+# Activate mouse cursor handling by Speedy and load default cursors
+func _init():
+	if not Engine.editor_hint:
+		Input.set_mouse_mode(
+			Input.MOUSE_MODE_HIDDEN
+		)
+	set_custom_mouse_cursor(
+		preload("res://addons/speedy_gonzales/images/arrow.png")
+	)
+
+
+# Handle mouse motions and switch cursor when needed
+#
+# ** Parameters **
+#
+# - event: The input event
+func _input(event):
+	if event is InputEventMouseMotion and \
+			(event as InputEventMouseMotion).relative != Vector2(0,0):
+		if not hidden and not keep_shape and \
+				current_shape != Input.get_current_cursor_shape():
+			_update_shape()
+		$Cursor.position = event.position - current_hotspot
+		
+
+# Set the custom mouse cursor
+#
+# ** Parameters **
+#
+# - image: The image to use for the mouse cursor
+# - shape: The shape id that the image should represent
+# - hotspot: The hotspot position of the cursor
+# - target_position: Warp the mouse cursor to this point
+func set_custom_mouse_cursor(
+	image: Texture, 
+	shape = Input.CURSOR_ARROW, 
+	hotspot: Vector2 = Vector2(0,0),
+	target_position = null
+):
+	textures[shape] = image
+	hotspots[shape] = hotspot
+	if target_position == null and get_viewport() != null:
+		target_position = get_viewport().get_mouse_position() - current_hotspot
+	elif target_position != null:
+		get_viewport().warp_mouse(target_position + hotspot)
+	if shape == current_shape:
+		current_hotspot = hotspots[shape]
+		$Cursor.texture = textures[shape]
+		$Cursor.position = target_position
+
+
+# Force the current mouse cursor to display the given shape
+#
+# ** Parameters **
+#
+# - shape (Input.CursorShape) the shape id to set
+func set_shape(shape: int):
+	current_shape = shape
+	current_hotspot = hotspots[shape]
+	$Cursor.texture = textures[current_shape]
+
+
+# Disable the mouse cursor
+#
+# ** Parameters **
+#
+# - value: Wether the mouse cursor is hidden or not
+func _set_hidden(value: bool):
+	hidden = value
+	if hidden:
+		$Cursor.texture = null
+	else:
+		_update_shape()
+
+
+# Update the cursor to reflect the current shape
+func _update_shape():
+	set_shape(Input.get_current_cursor_shape())
+	
